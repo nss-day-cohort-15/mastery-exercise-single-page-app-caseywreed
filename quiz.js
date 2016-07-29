c2886f7a03ff6824fca758a3d12f10e636a74175
Casey Reed <casey.w.reed@gmail.com>
2016-07-29 13:19:11 -0500

    SOME data being thrown to dom

--- quiz.js
+++ quiz.js
@@ -2,19 +2,21 @@ CarLot = ( function (carlot) {
 
   carlot.populatePage = function (inventory) {
     // Loop over the inventory and populate the page
-    carlot.getInventory()
+    console.log(inventory)
     var outputElement = document.getElementById("outputElement");
-    inventory.forEach( function (car) {
-      car.model
+    inventory.cars.forEach( function (car) {
+      console.log(car)
+      outputElement.innerHTML += `<h1>${car.make}</h1>`
 
     })
     // Now that the DOM is loaded, establish all the event listeners needed
-    carlot.activateEvents();
+    // carlot.activateEvents();
     }
 
 carlot.loadInventory(carlot.populatePage)
 
   return carlot
+
 }(CarLot));
 
 
