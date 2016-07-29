43831b229b03692a7e2a94ba8040f5b6295a1da9
Casey Reed <casey.w.reed@gmail.com>
2016-07-29 11:15:07 -0500

    XHR loading done. Loads array of objects into private array

--- CarLot.js
+++ CarLot.js
@@ -7,12 +7,15 @@ var CarLot = (function () {
     },
     loadInventory: function (callback) {
       var inventoryLoader = new XMLHttpRequest();
-
+      inventoryLoader.open('GET', 'inventory.json')
       inventoryLoader.addEventListener("load", function () {
-
-      });
+        inventory = JSON.parse(event.target.responseText);
+        // console.log(inventory)
+      })
+      inventoryLoader.send();
     }
   };
 
-})();
+CarLot.loadInventory()
 
+})();
