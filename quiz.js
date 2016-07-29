20a6cebe05e1371d44ab36e275c944c083680e72
Casey Reed <casey.w.reed@gmail.com>
2016-07-29 15:28:48 -0500

    More failed alignment tweaks. Moving on to event listeners

--- quiz.js
+++ quiz.js
@@ -7,14 +7,14 @@ CarLot = ( function (carlot) {
     inventory.cars.forEach( function (car) {
       console.log(car)
       outputElement.innerHTML += `
-        <article class="col-md-3 singleCar" style="border: 10px solid ${car.color}">
+        <article class="col-md-3 col-md-3-offset-1 singleCar" style="border: 10px solid ${car.color}">
         <table class="table table-striped">
         <tr><td><span class="bold">Make:</span> ${car.make}</td></tr>
         <tr><td><span class="bold">Model:</span> ${car.model}</td></tr>
         <tr><td><span class="bold">Year:</span> ${car.year}</td></tr>
         <tr><td><span class="bold">$${car.price}</span></td></tr>
         <tr><td><span class="bold">Color:</span> ${car.color}</td></tr>
-        <tr><td><span class="bold">Purhcased:</span> ${car.purchased}</td></tr>
+        <tr><td><span class="bold">Purchased:</span> ${car.purchased}</td></tr>
         <tr><td><span class="bold">Description:</span> ${car.description}</td></tr>
         </table>
         </article>
@@ -22,7 +22,7 @@ CarLot = ( function (carlot) {
 
     })
     // Now that the DOM is loaded, establish all the event listeners needed
-    // carlot.activateEvents();
+    carlot.activateEvents();
     }
 
 carlot.loadInventory(carlot.populatePage)
