2bd29890883c8e97792e8717548d304c38097378
Casey Reed <casey.w.reed@gmail.com>
2016-07-31 19:53:31 -0500

    Really close. Trying to avoid infinite loops while traversing nodes

--- selectCar.js
+++ selectCar.js
@@ -4,6 +4,29 @@ CarLot = ( function (carlot) {
     console.log(car)
     console.log(color)
     var node = car.parentNode;
+    console.log(node)
+    // while (node != null) {
+    //   console.log("inside the node loop")
+    //   if (node.classList) {
+    //     console.log("inside node loop class list section")
+    //     if (node.classList.contains("singleCar")) {
+    //       console.log("ALTER THE DAMN CAR")
+    //     }
+    //   }
+    // }
+    // while (node != null) {
+    //   console.log("node is not null")
+    //   // If a class exists on the current node
+    //   if (node.classList) {
+    //     // Traversed to a 'car'
+    //     if (node.classList.contains('singleCar')) {
+    //       console.log("inside the SINGLECAR node loop")
+    //       node.style.border = '6px solid';
+    //       node.style.backgroundColor = color;
+    //       break;
+    //     }
+    //   }
+    // }
   }
 
   // carlot.resetTheCars = function () {},
