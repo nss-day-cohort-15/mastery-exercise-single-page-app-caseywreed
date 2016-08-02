24b2fe685918a0f4ac98e819e6611d31ab491496
Casey Reed <casey.w.reed@gmail.com>
2016-08-02 09:13:21 -0500

    Reset Cars is working

--- selectCar.js
+++ selectCar.js
@@ -1,21 +1,22 @@
 CarLot = ( function (carlot) {
 
   carlot.selectYourCar = function (car, color) {
-    // resetCars();
-    console.log(car)
+    resetCars();
+    // console.log(car)
     // console.log(color)
     var node = event.target.closest(".singleCar");
     node.classList.toggle("styledCar")
 
 
-    // function resetCars () {
-    //   var cars = document.querySelectorAll(".singleCar")
-    //   console.log("resetCars is running")
-    //   console.log(cars)
-    //   cars.forEach( e => {
-    //     car.classList.remove("styledCar")
-    //   })
-    // }
+    function resetCars () {
+      var cars = document.querySelectorAll(".styledCar")
+      console.log("resetCars is running")
+      console.log(cars)
+      cars.forEach( function (car) {
+        console.log("removing styling")
+        car.classList.remove("styledCar")
+      })
+    }
 
   }
 
